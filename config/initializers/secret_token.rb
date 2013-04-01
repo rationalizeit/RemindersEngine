d73f23464d963bf85e1e95cef9125c5614bf953d
unknown <Faraaz Khan@NYO000073497001.ads.mckinsey.com>
2013-04-01 11:01:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemindersEngine::Application.config.secret_token = '015207ba8bc05bd50f88e21c28204423adae45bc9e569e19016426ec425e92d78292e0bf801bddb2f3b7263b132fe2c567d78fe2abe4fb8c35904ceed17c4db1'
